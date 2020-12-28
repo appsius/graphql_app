bab9ca013450d0fed956a76ee38f832e2d9d4a34
apsius <38158216+appsius@users.noreply.github.com>
2020-12-28 16:54:58 +0300

    Add items to Apollo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,7 @@ import { auth } from '../../firebase/firebase.utils';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { default as CartIcon } from '../cart-icon/cart-icon.container';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { default as CartDropdown } from '../cart-dropdown/cart-dropdown.container';
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
